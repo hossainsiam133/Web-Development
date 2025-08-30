80487e6166672a338ae50f911fdebd7e59812b4a
hossainsiam133 <hsiam133946@gmail.com>
2025-08-30 21:17:52 +0600

    Completed Class 6 and 7

--- 5_Javascript/Class_06/Practice_2/script.js
+++ 5_Javascript/Class_06/Practice_2/script.js
@@ -1,2 +1,19 @@
-let atr = document.querySelector("h2");
-console.log(atr.getAttribute())
+// Q1
+let body = document.querySelector("body")
+let btn = document.createElement("button")
+btn.innerText = "Click Me"
+btn.style.backgroundColor = "red"
+btn.style.color = "white"
+body.prepend(btn)
+
+// Q2
+let div = document.querySelector("p")
+let x = prompt("Enter a Number: ")
+if (x & 1) {
+    // div.setAttribute("class","graph")
+    div.classList.add("graph")
+    // div.classList.remove("graph")
+}
+else {
+    // div.remove()
+}
